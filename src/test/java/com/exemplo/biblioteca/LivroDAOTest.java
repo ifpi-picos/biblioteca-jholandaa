b83aa4af8a699cc87c36b96afe7b48236fd8649f
Juliana Holanda <168015141+jholandaa@users.noreply.github.com>
2025-02-23 23:13:20 -0300

    Update LivroDAOTest.java

--- src/test/java/com/exemplo/biblioteca/LivroDAOTest.java
+++ src/test/java/com/exemplo/biblioteca/LivroDAOTest.java
@@ -8,13 +8,9 @@ public class LivroDAOTest {
     public void testInserirLivro() {
         LivroDAO livroDAO = new LivroDAO();
 
-        // Criando um objeto Livro corretamente
         Livro livro = new Livro(1984, "George Orwell", "LIV001", 1949); 
         
-        // Chamando o método corretamente
         livroDAO.inserirLivro(livro);
-
-        // Se chegou até aqui sem erros, o teste passou
         assertTrue(true);
     }
 }
